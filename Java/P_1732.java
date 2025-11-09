b35189f9ebe0ecb2ee7119440ab070e8b358d50e
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-11-09 15:27:00 +0530

    Create P_1732.java

---
+++ Java/P_1732.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int maxAltitude = 0;
+        int currAltitude = 0;
+        for (int g : gain) {
+            currAltitude += g;
+            maxAltitude = Math.max(maxAltitude, currAltitude);
+        }
+        return maxAltitude;
+    }
+}
